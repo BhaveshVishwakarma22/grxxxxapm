936a769e98482ec453b584545524b098658b0624
Bhavesh Vishwakarma <91364943+BhaveshVishwakarma22@users.noreply.github.com>
2023-08-15 09:15:57 +0530

    Update ListingsRoutes.js
    Added route to getUserListing()

--- routes/ListingsRoutes.js
+++ routes/ListingsRoutes.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const {getAllUserListing, addToMyListings, getUserListing, deleteListing, deleteAllListing} = require("../controllers/userListingController");
+const {getAllUserListing, addToMyListings, getUserListing, deleteListing, deleteAllListing, getUserListingItems} = require("../controllers/userListingController");
 
 
 router.route(`/`).get(getAllUserListing).post(addToMyListings).delete(deleteListing);
 
 router.route(`/all`).delete(deleteAllListing);
+
+router.route(`/item/x`).patch(getUserListingItems);
 
 router.route(`/:id`).patch(getUserListing);
 

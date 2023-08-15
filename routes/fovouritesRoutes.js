af18ad7071cf6c203b4c68d5edc3c176e673a564
Bhavesh Vishwakarma <91364943+BhaveshVishwakarma22@users.noreply.github.com>
2023-08-15 08:30:31 +0530

    Update fovouritesRoutes.js
    Added Route /item/x

--- routes/fovouritesRoutes.js
+++ routes/fovouritesRoutes.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const {getAllFavorites, addToFavorites, getUserFavorite, deleteFavorites, deleteAllFavorites} = require("../controllers/favoriteController");
+const {getAllFavorites, addToFavorites, getUserFavorite, deleteFavorites, deleteAllFavorites, getUserFavoriteItems} = require("../controllers/favoriteController");
 
 
 router.route(`/`).get(getAllFavorites).post(addToFavorites).delete(deleteFavorites);
 
 router.route(`/all`).delete(deleteAllFavorites);
+
+router.route(`/item/x`).patch(getUserFavoriteItems);
 
 router.route(`/:id`).patch(getUserFavorite);
 

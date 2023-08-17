0ba0d5ca08d1bb71d937b54cb48c4aaa83123725
Bhavesh Vishwakarma <91364943+BhaveshVishwakarma22@users.noreply.github.com>
2023-08-17 05:37:31 +0530

    Update houseSellRoutes.js
    Added Route to `/item/:location`  getItemsByLocation

--- routes/houseSellRoutes.js
+++ routes/houseSellRoutes.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const {getAllHouseSell, getUserHouseSell, addHouseSell, updateHouseSell, deleteHouseSell} = require("../controllers/houseSellController");
+const {getAllHouseSell, getUserHouseSell, addHouseSell, updateHouseSell, deleteHouseSell, getItemsByLocation} = require("../controllers/houseSellController");
 
 
 router.route(`/`).get(getAllHouseSell).post(addHouseSell).put(updateHouseSell).delete(deleteHouseSell);
+
+router.route(`/item/:location`).get(getItemsByLocation);
 
 router.route(`/:id`).patch(getUserHouseSell);
 
